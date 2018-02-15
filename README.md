2e3c383e28364c5004b7d9e6222541d0890e3a5a
Jae Chong <jaechong115@gmail.com>
2018-02-14 20:19:27 -0500

    Initial check-in for project 1 to test.

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
+# Project 1
++ By: *Jae Yong Chong*
++ Production URL: <http://p1.jaechong.com>
+
+## Outside resources
++ I had to find references for some CSS tags: how to justify the text: https://www.w3schools.com/cssref/css3_pr_text-justify.asp
++ how to center the image:
+https://stackoverflow.com/questions/7055393/center-image-using-text-align-center
++ For quotes: https://www.brainyquote.com/topics/best
++ for random entries out of an array: http://php.net/manual/en/function.array-rand.php
+
+## Notes for instructor
+*Thank you for the extension.  My mom went to hospital when I finished watching all the lectures and I thought I could just do the project in less than a day, however, when I came back after little more than a week of absent, I complete forgot everything and had to rewatch the videos to do the project.  Barely finished on time I promised...*
 # p1
 CSCI E15 - Project 1
---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Jae Chong: Indtroduction</title>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="css/jaechong.css" />
+</head>
+
+<body>
+    <div class="center">
+<h1>Jae Chong</h1>
+<img src='images/jaechong.JPG' alt="Jae Chong">
+    <p>My name is Jae Chong.  I have worked in the personal computer industry as a software engineer for more than 35 years and have recently retired.</p>  
+    <p>I had too many things to do when I retired. Drawing, painting, cartooning, woodworking, pottery making and etc have been my passion since in my childhood. I was keen to learn new things like playing piano, guitar, and saxophone. I also looked forward to play squash, badminton, table tennis, and soccer without having to rush back to work. To make my retirement more meaningful, I was eager to do lots of community volunteering work. After making myself busy enjoying all the things mentioned above for about 6 months, I just felt I should contribute more to the society with the skills that I have while I am still young.</p>
+    <p>I took CSCI E3 and E12, and now I am taking CSCI E15 and E31 this term at HES.  I hope to learn both front and back end of the web development and hopely to help some charity organizations and friends.  I spent most of my career working on the low system level programming so it is really nice to explore in this high level programming world.  I always thought programming was my job but I am realizing how fun it is to program after more than 35 years of programming.</p>
+    </div>
+    <div>
+    </div>
+    </body>
+</html>
